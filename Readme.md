7a02526293b49c6b5a760f7f5e6c8c3b896007ce
James Sumners <james.sumners@gmail.com>
2014-10-07 10:27:31 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,40 @@
+# failover-lb
+
+This repository provides a simple build script to automate creating RPMs for
+[HAProxy](http://haproxy.org/) and [Keepalived](http://keepalived.org/).
+
+This repository includes files from the following projects:
+
+* [https://github.com/bluerail/haproxy-centos/](https://github.com/bluerail/haproxy-centos/)
+* [https://github.com/santisaez/powerstack/](https://github.com/santisaez/powerstack/)
+
+## Building
+
+1) Clone this repository
+2) Run the `build.sh` script
+
+That's really all there is to it. Running `build.sh` without a parameter will build both
+HAProxy and Keepalived. You can choose to build a single package by specifying the name:
+
+    ./build.sh haproxy
+
+or
+
+    ./build.sh keepalived
+
+During the build process a 'rpmbuild' directory will be created in the root directory
+of the repository. Once the builds are complete, you can find the RPMs in the
+'rpmbuild/RPMS/' directory.
+
+## License
+
+[http://jsumners.mit-license.org](http://jsumners.mit-license.org)
+
+THE MIT LICENSE (MIT)
+Copyright © 2014 James Sumners <james.sumners@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+keepalivedVersion="1.2.13"
+haproxyVersion="1.5.4"
+
+keepalivedUrl="http://keepalived.org/software/keepalived-${keepalivedVersion}.tar.gz"
+haproxyUrl="http://www.haproxy.org/download/1.5/src/haproxy-${haproxyVersion}.tar.gz"
+
+which wget > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Aborting. Cannot continue without wget."
+  exit 1
+fi
+
+which rpmbuild > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Aborting. Cannot continue without rpmbuild. Please install the rpmdevtools package."
+  exit 1
+fi
+
+if [ "$1" != "" ]; then
+  product=$(echo $1 | tr '[:upper:]' '[:lower:]')
+else
+  product="both"
+fi
+
+echo ${product} | grep -E '^(both|haproxy|keepalived)$' 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Invalid product name. Valid options are 'keepalived', 'haproxy', or 'both'."
+  echo "For example:"
+  echo -e "\t./build.sh keepalived"
+  exit 1
+fi
+
+
+# Let's get down to business
+TOPDIR=$(pwd)
+
+if [ -e rpmbuild ]; then
+  rm -rf rpmbuild/* 2>&1 > /dev/null
+fi
+
+echo "Creating RPM build path structure..."
+mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS,tmp}
+
+# haproxy
+echo "${product}" | grep -E '^(both|haproxy)$' 2>&1 > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Building HAProxy RPM ..."
+  cp ${TOPDIR}/files/haproxy.{cfg,init,logrotate} ${TOPDIR}/rpmbuild/SOURCES/
+  cp ${TOPDIR}/files/haproxy.spec ${TOPDIR}/rpmbuild/SPECS/
+
+  sed -i 's/~haproxyVersion~/'${haproxyVersion}'/' ${TOPDIR}/rpmbuild/SPECS/haproxy.spec
+  
+  cd ${TOPDIR}/rpmbuild/SOURCES/
+  wget ${haproxyUrl}
+  
+  cd ${TOPDIR}/rpmbuild/
+  rpmbuild --define "_topdir ${TOPDIR}/rpmbuild" -ba "SPECS/haproxy.spec"
+fi
+
+# keepalived
+echo "${product}" | grep -E '^(both|keepalived)$' 2>&1 > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Building Keealived RPM ..."
+  cp ${TOPDIR}/files/keepalived.spec ${TOPDIR}/rpmbuild/SPECS/
+
+  sed -i 's/~keepalivedVersion~/'${keepalivedVersion}'/' ${TOPDIR}/rpmbuild/SPECS/keepalived.spec
+
+  cd ${TOPDIR}/rpmbuild/SOURCES/
+  wget ${keepalivedUrl}
+
+  tar zxf keepalived-${keepalivedVersion}.tar.gz
+  cd keepalived-${keepalivedVersion}/keepalived/etc/init.d/
+  cp keepalived.rh.init ${TOPDIR}/rpmbuild/SOURCES/keepalived.init
+
+  cd ${TOPDIR}/rpmbuild/
+  rpmbuild --define "_topdir ${TOPDIR}/rpmbuild" -ba "SPECS/keepalived.spec"
+fi
