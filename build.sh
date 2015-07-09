7fdf7751dddcf8d9e1394b2d264f928ecb9a5e3e
James Sumners <james.sumners@gmail.com>
2015-07-09 09:24:13 -0400

    Bump version numbers

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-keepalivedVersion="1.2.15"
-haproxyVersion="1.5.10"
+keepalivedVersion="1.2.19"
+haproxyVersion="1.5.14"
 
 keepalivedUrl="http://keepalived.org/software/keepalived-${keepalivedVersion}.tar.gz"
 haproxyUrl="http://www.haproxy.org/download/1.5/src/haproxy-${haproxyVersion}.tar.gz"
